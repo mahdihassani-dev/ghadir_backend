6140cd89732d96fe5210a43ee670d966375b99f8
mahdi <m.hassani4951383@gmail.com>
2026-06-04 16:37:52 +0330

    D

--- ghadir/settings.py
+++ ghadir/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z-=uj5(i17nu!dnpz4de8n9#7+i6z%d^%=r2-0=^yx&w__=#-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
